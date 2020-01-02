c1b0dd785193d1ad7cd0ce5694bdab35930d697d
Ben Wu <benjaminwu@college.harvard.edu>
2020-01-02 16:33:39 -0500

    simple example rendering on template

--- mysite/graphs/urls.py
+++ mysite/graphs/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from graphs import views
+from graphs.dash_apps.finished_apps import simpleexample
 # from mysite.graphs.dash_apps.finished_apps import boxplot
 urlpatterns = [
     path('', views.graphs, name='graphs'),
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_plotly_dash.apps.DjangoPlotlyDashConfig',
-    # 'channels',
-    # 'channels_redis',
+    'channels',
+    'channels_redis',
 ]
 
 # CHANNEL_LAYERS = {
@@ -127,7 +127,33 @@ USE_L10N = True
 
 USE_TZ = True
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+ASGI_APPLICATION = 'mysite.routing.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379), ],
+        },
+    },
+}
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django_plotly_dash.finders.DashAssetFinder',
+    'django_plotly_dash.finders.DashComponentFinder',
+]
+
+PLOTLY_COMPONENTS = [
+    'dash_core_components',
+    'dash_html_components',
+    'dash_renderer',
+
+    'dpd_components'
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATICFILES_LOCATION = 'static'
